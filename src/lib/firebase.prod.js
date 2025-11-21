738aed68dbec98fce4441d6b5cbb9b036b88a0d1
Ganesh <ganeshkumarburra2@gmail.com>
2025-11-21 15:40:34 +0530

    Replacing creds

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -7,12 +7,12 @@ import { seedDatabase } from '../seed';
 
 //we need a config here, put your api details here 
 const config = {
-    apiKey: "AIzaSyD6rfFIRNC4j0YoRtZGi6vLlJ0M2baoffE",
-    authDomain: "netflix-react-9480e.firebaseapp.com",
-    projectId: "netflix-react-9480e",
-    storageBucket: "netflix-react-9480e.appspot.com",
-    messagingSenderId: "124358470674",
-    appId: "1:124358470674:web:e265ab9bc66f7c4e9a3ccb"
+    apiKey: "AIzaSyDTlZfbbBcrsr2MFNimgp-R_KheOmVmNZA",
+    authDomain: "netflix-clone-e55e4.firebaseapp.com",
+    projectId: "netflix-clone-e55e4",
+    storageBucket: "netflix-clone-e55e4.firebasestorage.app",
+    messagingSenderId: "92955344134",
+    appId: "1:92955344134:web:855a749aab21263fbff287"
 };
 
 const firebase = Firebase.initializeApp(config);
